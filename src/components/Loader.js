65a4a6de5a1ec1cad7780a038cab50a17b2e6167
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-09-12 20:57:51 +0100

    message

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -6,8 +6,8 @@ import loader from '../assets/loading.gif'
 const Loader = () => {
   return (
     <div style={{textAlign: 'center', marginTop: '200px'}}>
-        <img src={logo} alt="" width='150px'style={{display: 'block', margin: 'auto'}}/>
-        <img src={loader} alt="loading" width='60px'style={{display: 'block', margin: 'auto'}}/>
+        <img src={logo} alt="" width='100px'style={{display: 'block', margin: 'auto'}}/>
+        <img src={loader} alt="loading" width='50px'style={{display: 'block', margin: 'auto'}}/>
     </div>
   )
 }
